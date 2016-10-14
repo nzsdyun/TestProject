961a03f1007390a45d8c7324876d8cacf7dfbd4b
nzsdyun <1799058367@qq.com>
2016-10-14 15:01:28 +0800

    add use path draw and draw text

--- grapgicsdemo/src/main/java/com/example/sky/grapgicsdemo/graphics/view/DifferentViews.java
+++ grapgicsdemo/src/main/java/com/example/sky/grapgicsdemo/graphics/view/DifferentViews.java
@@ -1,13 +1,16 @@
 package com.example.sky.grapgicsdemo.graphics.view;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -56,7 +59,167 @@ public class DifferentViews extends View {
         onDrawPoint(canvas);
         //draw rect
         onDrawRect(canvas);
+        //draw path
+        onDrawPath(canvas);
+        //draw text
+        onDrawText(canvas);
         super.onDraw(canvas);
+    }
+
+    private void onDrawText(Canvas canvas) {
+        /*
+        Paint textPaint = new Paint();
+        //FIXME:set normal
+        textPaint.setColor(Color.RED);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setStrokeWidth(5);
+        textPaint.setAntiAlias(true);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTextSize(20);
+        //FIXME: set style
+        // bold
+        textPaint.setFakeBoldText(true);
+        //under line
+        textPaint.setUnderlineText(true);
+        //strikeThruText
+        textPaint.setStrikeThruText(true);
+        //tilt effect
+        textPaint.setTextSkewX(0.25f);
+        //FIXME: set other
+        //horizontal tensile font
+        textPaint.setTextScaleX(2);*/
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.RED);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setStrokeWidth(5);
+        textPaint.setAntiAlias(true);
+        textPaint.setTextSize(80);
+        canvas.drawText("Android程序员", 10, 800, textPaint);
+        textPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawText("Android程序员", 10, 900, textPaint);
+        textPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawText("Android程序员", 10, 1000, textPaint);
+        //style
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setTextSize(40);
+        textPaint.setTextSkewX(0.25f);
+        textPaint.setUnderlineText(true);
+        textPaint.setFakeBoldText(true);
+        textPaint.setStrikeThruText(true);
+        canvas.drawText("Android程序员", 10, 1050, textPaint);
+        textPaint.setTextSkewX(-0.25f);
+        canvas.drawText("Android程序员", 300, 1050, textPaint);
+        //other
+        textPaint.setTextScaleX(1);
+        canvas.drawText("Android程序员", 10, 1150, textPaint);
+        textPaint.setTextScaleX(2);
+        canvas.drawText("Android程序员", 10, 1150, textPaint);
+        //specify the text location map
+        textPaint.setTextSize(40);
+        canvas.drawPosText("程序",
+                new float[] {
+                        10, 1200,
+                        10, 1240
+                }, textPaint);
+        //FIXME:　draw text on path
+        Path circlePath = new Path();
+        textPaint.setStyle(Paint.Style.STROKE);
+        circlePath.addCircle(200, 1400, 100, Path.Direction.CW);
+        canvas.drawPath(circlePath, textPaint);
+//        circlePath.reset();
+        textPaint.setColor(Color.GREEN);
+        textPaint.setStyle(Paint.Style.FILL);
+        canvas.drawTextOnPath("Android程序员", circlePath, 0, 0, textPaint);
+        Path circlePath1 = new Path();
+        textPaint.setStyle(Paint.Style.STROKE);
+        circlePath1.addPath(circlePath, 300, 0);
+        canvas.drawPath(circlePath1, textPaint);
+        textPaint.setColor(Color.GREEN);
+        textPaint.setStyle(Paint.Style.FILL);
+        canvas.drawTextOnPath("Android程序员", circlePath1, 30, 40, textPaint);
+        //set font
+//        AssetManager am = mContext.getAssets();
+//        Typeface fontTypeface = Typeface.createFromAsset(am, "fonts/jian_luobo.ttf");
+//        textPaint.setTypeface(fontTypeface);
+//        textPaint.setTextSize(20);
+//        canvas.drawText("Android程序员", 20, 1500, textPaint);
+
+    }
+
+    /**
+     * use path can draw any graphics
+     * @param canvas
+     */
+    private void onDrawPath(Canvas canvas) {
+        //FIXME: drawLineByPath
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        Path lp = new Path();
+        //set the beginning of the point
+        lp.moveTo(10, 520);
+        //the relative value of a previous point
+        lp.rMoveTo(40, 0);
+        //move to next point
+        lp.lineTo(100, 520);
+        lp.rLineTo(20, 20);
+        lp.lineTo(100, 560);
+        //form a closed loop, ending
+        lp.close();
+        canvas.drawPath(lp, paint);
+        //FIXME:drawArcByPath
+        //reset paint
+        paint.reset();
+        Path arcPath = new Path();
+        RectF arcRect = new RectF(10, 570, 200, 650);
+        //FIXME:counterclockwise rectangle
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(3);
+        paint.setStyle(Paint.Style.STROKE);
+        arcPath.addRect(arcRect, Path.Direction.CCW);
+        canvas.drawPath(arcPath, paint);
+        //use green draw arc
+        arcPath.reset();
+        paint.setColor(Color.GREEN);
+        arcPath.addArc(arcRect, 0, 240);
+        canvas.drawPath(arcPath, paint);
+        //FIXME: draw circle by pah
+        Path path = new Path();
+        //clockwise draw circle
+        paint.setAntiAlias(true);
+        path.addCircle(400, 600, 30, Path.Direction.CW);
+        canvas.drawPath(path, paint);
+        //FIXME: copy path
+        paint.setColor(Color.RED);
+        Path circlePath = new Path();
+        circlePath.addPath(path, 40, 0);
+        canvas.drawPath(circlePath, paint);
+        //FIXME: draw oval
+        path.reset();
+        RectF ovalRectF = new RectF(10, 680, 120, 750);
+        //FIXME: draw round rectangle
+        paint.setColor(Color.RED);
+        path.addRoundRect(ovalRectF, 10, 10, Path.Direction.CCW);
+        canvas.drawPath(path, paint);
+        path.reset();
+        paint.setColor(Color.GREEN);
+        path.addOval(ovalRectF, Path.Direction.CCW);
+        canvas.drawPath(path, paint);
+        path.reset();
+        path.addRoundRect(
+                new RectF(150, 680, 260, 750),
+                //specify the size of the rounded corners
+                new float[] {
+                        5, 10,
+                        10, 15,
+                        15, 20,
+                        20, 25
+                },
+                Path.Direction.CW
+        );
+        paint.setColor(Color.GREEN);
+        canvas.drawPath(path, paint);
     }
 
     private void onDrawRect(Canvas canvas) {
